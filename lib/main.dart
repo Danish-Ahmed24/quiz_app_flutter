09e74540efa43ae64dd2b6342ce8a2a5b39f5d96
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-19 23:19:24 +0500

    Challenge1: text

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,9 @@
+// bg color - rgb(74, 0, 147)
+// button color - rgb(86, 0, 156)
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(home: Text("Learn Flutter the fun way")),
+  );
+}
