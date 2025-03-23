27fdbb01a7e667b8932c8a98e977f74a0094b0f1
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-23 18:08:34 +0500

    quiz and question_screen initials

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,8 @@
 // bg color - rgb(74, 0, 147)
 // button color - rgb(86, 0, 156)
 import 'package:flutter/material.dart';
-import 'package:quiz_app/home_page.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomLeft,
-              colors: [
-                const Color.fromARGB(255, 18, 45, 220),
-                const Color.fromARGB(255, 152, 91, 218),
-              ],
-            ),
-          ),
-          child: HomePage(),
-        ),
-      ),
-    ),
-  );
+  runApp(Quiz());
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/home_page.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,6 +13,22 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   @override
   Widget build(context) {
-    return Text("hi");
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+              colors: [
+                const Color.fromARGB(255, 18, 45, 220),
+                const Color.fromARGB(255, 152, 91, 218),
+              ],
+            ),
+          ),
+          child: HomePage(),
+        ),
+      ),
+    );
   }
 }
